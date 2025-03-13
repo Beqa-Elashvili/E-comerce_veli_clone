bee468c7936bdb7a3a36a8e1e8ef59413584a19b
Beqael <beqaelashvili3@gmail.com>
2025-03-13 14:44:11 +0400

    add_cart-modal

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -2,13 +2,14 @@
 
 import {
   Heart,
-  Menu,
   Moon,
   Sun,
   UserRound,
   ShoppingCart,
   SearchX,
   Search,
+  X,
+  Trash,
 } from "lucide-react";
 import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
@@ -16,32 +17,33 @@ import { useAppDispatch, useAppSelector } from "@/app/redux";
 import {
   setIsCartItemUnauthentificated,
   setISDarkMode,
-  setIsSideBarCollapsed,
   setIsWishlist,
 } from "@/redux/globalSlice";
 import { LogOut } from "lucide-react";
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
-import { DotLoader } from "react-spinners";
-import { Category, Product } from "@/app/types/globalStateTypes";
+import { DotLoader, GridLoader } from "react-spinners";
+import { CartItem, Product } from "@/app/types/globalStateTypes";
 import axios from "axios";
 import { usePathname } from "next/navigation";
 import { SyncLoader } from "react-spinners";
 import useHandleCartquantity from "@/app/hooks/useHandleCartQuantity";
-import CarouselComp from "../Carousel/Carousel";
 import { useRouter } from "next/navigation";
-import { TableOfContents } from "lucide-react";
+import { Triangle } from "lucide-react";
+import useAddToCartMain from "@/app/hooks/addToCartMain";
+import useDeleteCartItem from "@/app/hooks/useDeleteCartItem";
+import useAddToCart from "@/app/hooks/addToCart";
+import useHandleQuantityIncart from "@/app/hooks/useHandleQuantityInCart";
 
 const Navbar = () => {
   const { status } = useSession();
   const dispatch = useAppDispatch();
   const pathname = usePathname();
   const router = useRouter();
-  const { handleCartQuantity } = useHandleCartquantity();
-  const categories = useAppSelector((state) => state.categories.Categories);
+  const { handleCartQuantity, handleTotalPrice } = useHandleCartquantity();
+  const { handleQuantityIncart } = useHandleQuantityIncart();
 
   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
-
   const toggleTheme = () => {
     dispatch(setISDarkMode(!isDarkMode));
   };
@@ -58,6 +60,14 @@ const Navbar = () => {
   const [searchResults, setSearchResults] = useState<Product[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [showResults, setShowResults] = useState<boolean>(false);
+  const [showCart, setShowCart] = useState<boolean>(false);
+  const { addToCartWithVariants } = useAddToCartMain();
+  const { addToCart, loadingStates, setLoadingStates } = useAddToCart();
+  const { deleteCartItem } = useDeleteCartItem();
+
+  const cart = useAppSelector(
+    (state) => state.global.isCartItemUnauthentificated
+  );
 
   async function getSearchResult(query: string) {
     try {
@@ -75,8 +85,10 @@ const Navbar = () => {
       setLoading(false);
     }
   }
+  const user = useAppSelector((state) => state.user.user);
 
   const searchRef = useRef<HTMLDivElement>(null);
+  const cartRef = useRef<HTMLDivElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -116,9 +128,104 @@ const Navbar = () => {
   }, [searchResults, showResults]);
 
   useEffect(() => {
+    if (showCart) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (cartRef.current && !cartRef.current.contains(event.target as Node)) {
+        setShowCart(false);
+        cartRef.current?.blur();
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.body.style.overflow = "auto";
+    };
+  }, [cart, showCart]);
+  useEffect(() => {
+    setShowCart(false);
+    setShowResults(false);
+  }, [pathname]);
+
+  useEffect(() => {
     setValue("");
     setSearchResults([]);
   }, [pathname]);
+
+  const handleAddToCart = async (product: Product) => {
+    if (status === "unauthenticated") {
+      const productWithVariant = {
+        ...product,
+        selectedColor: product.selectedColor,
+        selectedSize: product.selectedSize,
+        VariantStock: product.VariantStock,
+      };
+      addToCart(user?.id as unknown as string, productWithVariant, 1);
+    } else {
+      console.log(product);
+      addToCartWithVariants({ ...product, id: product.productId });
+    }
+  };
+
+  const handleCartItemId = async (product: Product) => {
+    if (status === "unauthenticated") {
+      setLoadingStates((prev) => ({
+        ...prev,
+        [product.id]: true,
+      }));
+      if (product.selectedColor !== "" || product.selectedSize !== "") {
+        const isCart = cart?.find(
+          (item: Product) =>
+            item.id === product.id &&
+            (item.selectedColor === product.selectedColor ||
+              !product.selectedColor) &&
+            (item.selectedSize === product.selectedSize ||
+              !product.selectedSize)
+        );
+
+        isCart && (await deleteCartItem(isCart, true));
+        setLoadingStates((prev) => ({
+          ...prev,
+          [product.id]: false,
+        }));
+      } else {
+        const isCart = cart?.find(
+          (item: Product) =>
+            item.productId === product.productId &&
+            (item.selectedColor === product.selectedColor ||
+              !product.selectedColor) &&
+            (item.selectedSize === product.selectedSize ||
+              !product.selectedSize)
+        );
+        isCart && (await deleteCartItem(isCart, true));
+        setLoadingStates((prev) => ({
+          ...prev,
+          [product.id]: false,
+        }));
+      }
+    } else {
+      try {
+        setLoadingStates((prev) => ({
+          ...prev,
+          [product.productId]: true,
+        }));
+        const isCart = cart?.find(
+          (item: Product) =>
+            item.productId === product.productId &&
+            (item.selectedColor === product.selectedColor ||
+              !product.selectedColor) &&
+            (item.selectedSize === product.selectedSize ||
+              !product.selectedSize)
+        );
+        isCart && (await deleteCartItem(isCart, true));
+        setLoadingStates((prev) => ({ ...prev, [product.productId]: false }));
+      } catch (error: unknown) {}
+    }
+  };
 
   return (
     <div
@@ -128,6 +235,12 @@ const Navbar = () => {
         ref={searchRef}
         className={` ${
           showResults ? "block" : "hidden"
+        } fixed bg-gray-800 inset-0 z-30 h-full bg-opacity-60`}
+      ></div>
+      <div
+        ref={cartRef}
+        className={` ${
+          showCart ? "block" : "hidden"
         } fixed bg-gray-800 inset-0 z-30 h-full bg-opacity-60`}
       ></div>
       <div className="flex justify-between w-full items-center gap-5">
@@ -212,19 +325,119 @@ const Navbar = () => {
             />
           </Link>
           <hr className="w-0 h-7 border border-solid border-gray-300 mx-2" />
-          <Link
-            href="/cart"
-            className=" relative group flex rounded-lg h-[40px] overflow-hidden p-2 items-center hover:ring-1 ring-white transition duration-300 gap-2"
-          >
-            <div className="relative flex  items-center cursor-pointer w-10 ">
-              <ShoppingCart />
-              <div className="absolute font-semibold bg-orange-400 text-xs right-1 rounded-full  px-[0.4rem] py-1 flex -top-1 leading-none">
-                {handleCartQuantity()}
+          <div className="relative">
+            <div
+              ref={cartRef}
+              onClick={() => setShowCart((prev) => !prev)}
+              className=" relative group flex rounded-lg h-[40px] overflow-hidden p-2 items-center hover:ring-1 ring-white transition duration-300 gap-2"
+            >
+              <div className="relative flex  items-center cursor-pointer w-10 ">
+                <ShoppingCart />
+                <div className="absolute font-semibold bg-orange-400 text-xs right-1 rounded-full  px-[0.4rem] py-1 flex -top-1 leading-none">
+                  {handleCartQuantity()}
+                </div>
               </div>
+              <p className="font-semibold">კალათა</p>
+              <div className="absolute pointer-events-none inset-0 bg-white/10 w-full h-full translate-x-full opacity-0 group-hover:opacity-100 group-hover:animate-light-move"></div>
             </div>
-            <p className="font-semibold">კალათა</p>
-            <div className="absolute pointer-events-none inset-0 bg-white/10 w-full h-full translate-x-full opacity-0 group-hover:opacity-100 group-hover:animate-light-move"></div>
-          </Link>
+            {showCart && (
+              <div ref={cartRef} className="relative z-30 w-full">
+                <div className="absolute bg-white top-7 w-96 right-2  rounded-lg">
+                  <div className=" flex justify-between p-2 items-center gap-12">
+                    <h1 className="text-lg font-semibold">ჩემი კალათა</h1>
+                    <X className="size-6  bg-gray-200 rounded-full p-1 cursor-pointer hover:bg-gray-300 transition duration-300" />
+                  </div>
+                  <hr />
+                  <div className="flex flex-col gap-2">
+                    {cart?.length !== 0 ? (
+                      <>
+                        {cart?.map((item: Product, index: number) => (
+                          <div key={item.id}>
+                            <div className="flex items-center p-2 justify-between">
+                              <div className="flex">
+                                <img
+                                  src={item.images[0].url}
+                                  className="size-14"
+                                  alt="image"
+                                />
+                                <div className="space-y-2">
+                                  <h1>{item.name}</h1>
+                                  <p>{item.price} ₾</p>
+                                </div>
+                              </div>
+                              <div>
+                                {cart?.length !== 0 && (
+                                  <>
+                                    <div className="flex items-center justify-center gap-4">
+                                      <div
+                                        onClick={() => handleAddToCart(item)}
+                                        className="h-8 w-8 cursor-pointer hover:bg-gray-200 rounded-full border flex items-center justify-center"
+                                      >
+                                        +
+                                      </div>
+                                      {loadingStates[
+                                        status === "unauthenticated"
+                                          ? item.id
+                                          : item.productId
+                                      ] ? (
+                                        <>
+                                          <GridLoader
+                                            color="#525fd16c"
+                                            size={4}
+                                            className="w-6"
+                                          />
+                                        </>
+                                      ) : (
+                                        <h1>{handleQuantityIncart(item)}</h1>
+                                      )}
+                                      <div
+                                        onClick={() => handleCartItemId(item)}
+                                        className="h-8 w-8 cursor-pointer hover:bg-gray-200 rounded-full border flex items-center justify-center"
+                                      >
+                                        -
+                                      </div>
+                                      <button
+                                        className="hover:text-gray-600"
+                                        onClick={() =>
+                                          deleteCartItem(item, false)
+                                        }
+                                      >
+                                        <Trash />
+                                      </button>
+                                    </div>
+                                  </>
+                                )}
+                              </div>
+                            </div>
+                            {index !== cart.length - 1 && <hr />}
+                            <div className="p-2 flex mt-2 items-center justify-between">
+                              <p>სულ: {handleTotalPrice()} ₾</p>
+                              <div className="flex gap-1">
+                                <button
+                                  onClick={() => router.push("/cart")}
+                                  className="flex bg-black hover:bg-gray-900 transition duration-300 text-white p-2 justify-center items-center rounded-lg"
+                                >
+                                  ნახვა ({cart.length})
+                                </button>
+                                <button className="flex bg-green-400 hover:bg-green-500 transition duration-300 justify-center p-2 items-center rounded-lg">
+                                  ყიდვა
+                                </button>
+                              </div>
+                            </div>
+                          </div>
+                        ))}
+                      </>
+                    ) : (
+                      <h1 className="p-2">შენი კალათა ცარიელია</h1>
+                    )}
+                  </div>
+                </div>
+                <span className="text-black absolute z-30  top-4 left-16">
+                  <Triangle className="text-white size-4 fill-white" />
+                </span>
+              </div>
+            )}
+          </div>
           <hr className="w-0 h-7 border border-solid border-gray-300 mx-3" />
           <div className="relative flex h-[40px]  hover:ring-1 p-2 ring-white transition duration-300 items-center  gap-2 font-semibold cursor-pointer rounded-md overflow-hidden group">
             {status === "authenticated" ? (
